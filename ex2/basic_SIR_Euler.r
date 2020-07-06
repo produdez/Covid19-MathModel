f118d5861285616b3ddb8220cb1a01de68579084
dirtyg2120 <zone2120vn@gmail.com>
2020-07-06 14:06:30 +0700

    add R code for calculate basic SIR model using Euler's method

---
+++ ex2/basic_SIR_Euler.r
@@ -0,0 +1,29 @@
+S <- c()
+I <- c()
+R <- c()
+
+S[1] <- 800
+I[1] <- 7
+R[1] <- 0
+
+t <- seq(0, 8, by = 1)
+beta <- 0.002
+gamma <- 0.5
+
+for (i in 1:(length(t)-1)) {
+  S[i+1] = S[i] - (beta * I[i] * S[i]) * 1
+  
+  I[i+1] = I[i] + (beta * I[i] * S[i] - gamma * I[i]) * 1
+  
+  R[i+1] = R[i] + (gamma * I[i]) * 1
+}
+plot(S~t, col="red")
+lines(S~t, col="red")
+lines(I~t, col="green")
+points(I~t, col="green")
+lines(R~t, col="blue")
+points(R~t, col="blue")
+
+text(7, 800, paste0("S(t) = ", round(S[9], 4)), col = "red")
+text(7, 750, paste0("I(t) = ", round(I[9], 4)), col = "green")
+text(7, 700, paste0("R(t) = ", round(R[9], 4)), col = "blue")
