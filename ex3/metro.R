b996eddcd3794bd9cbe3f8f82c5f77394f8d4a0a
cloverfield <cloverfield@hashbang.sh>
2020-07-22 17:01:46 +0700

    Update exercise 3

--- ex3/metro.R
+++ ex3/metro.R
@@ -1,39 +1,29 @@
-library(MASS)
+# Initial state for the Markov chain
+theta <- c(beta=0, gamma=0)
+func <- function(theta) prod(dnorm(theta, sd=100))
+niter <- 10000
+burnin <- 10
+sd <- 2.4 / sqrt(2) * 100
 
-rw.metro <- function(start, func, nsamples, pcov.scale=1, ...) {
-	opt <- optim(start, func, control=list(fnscale=-1), hessian=TRUE, ...)
-	if (opt$convergence != 0) stop(opt$message)
-	V <- -pcov.scale^2 * solve(opt$hessian)
-	# Dimension of state space
-	dim <- length(start)
-	# Initial state for the Markov chain
-	theta <- opt$par
-	ptheta <- func(theta, ...)
-	# Generate matrix containing the samples.
-	samples <- matrix(nrow=nsamples, ncol=dim)
-	colnames(samples) <- names(start)
-	# Generate uniform random numbers in advance, to save computation.
-	log.u <- log(runif(nsamples))
-	# Proposal is a multivariate normal distribution. Generate samples and
-	# later on use linearity property of Gaussian distribution
-	normal.shift <- mvrnorm(n=nsamples, mu=rep(0, dim), Sigma=V)
-	for (i in seq_len(nsamples)) {
-		# Sample a candidate
-		candidate <- theta + normal.shift[i, ]
-		# Calculate func of candidate and store it in case it gets accepted
-		log.r <- func(candidate, ...) - ptheta
-		if (log.u[i] < log.r) {
-			theta <- candidate
-			ptheta <- func(theta, ...)
-		}
-		samples[i, ] <- theta
+ptheta <- func(theta)
+# Generate matrix containing the samples. Initialize first sample with the starting value
+sample <- matrix(nrow=niter, ncol=2, dimnames=list(NULL, names(start)))
+# Generate uniform random numbers in advance, to save computation.
+u <- runif(niter)
+# Proposal is a multivariate normal distribution. Generate samples and
+# later on use linearity property of Gaussian distribution
+normal.shift <- matrix(rnorm(niter*2,sd=sd), nrow=niter, ncol=2)
+for (i in seq_len(niter)) {
+	# Sample a candidate
+	candidate <- theta + normal.shift[i, ]
+	# Calculate func of candidate and store it in case it gets accepted
+	r <- func(candidate) / ptheta
+	if (u[i] < r) {
+		theta <- candidate
+		ptheta <- func(theta)
 	}
-	return(samples)
+	if (i > burnin) sample[i, ] <- theta
 }
 
-samples <- rw.metro(
-	start = c(beta=0, gamma=0),
-	func = function(theta) sum(dnorm(theta, sd=10, log=TRUE)),
-	nsamples = 10000,
-	pcov.scale = 1
-)
+accept <- 1 - sum(apply(diff(samples) == 0, 1, FUN=all)) / (nrow(samples) - 1)
+cat("Acception rate is", accept, "\n")
